5710f2ca34e1f4fbc6ddc6598340c7dd937abb38
pr3cision <fivehundred93@hotmail.com>
2020-09-23 22:54:30 -0400

    mise a jours + insertion jquerry

--- Projet Bootstrap/JavaScript/JavaScript.js
+++ Projet Bootstrap/JavaScript/JavaScript.js
@@ -1,7 +1,62 @@
+// @ts-nocheck
+
+var i = 0;
+
+
+function htmlMove() {
+  if (i == 0) {
+    i = 1;
+    var elemHtml = document.getElementById("htmlBar");
+    var elemCss = document.getElementById("cssBar");
+    
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elemHtml.style.width = width + "%";
+        elemHtml.innerHTML = "HTML " + width  + "%";
+
+        elemCss.style.width = width + "%";
+        elemCss.innerHTML = "HTML " + width  + "%";
+      }
+    }
+  }
+}
+
+function cssMove() {
+
+    if (i == 0) {
+      i = 1;
+      var elemCss = document.getElementById("cssBar");
+        console.log(elemCss)
+      var width = 0;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 80) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elemCss.style.width = width + "%";
+          elemCss.innerHTML = "Css " + width  + "%";
+        }
+      }
+    }
+  }
+
 $(function(){
     $(".navbar a, footer a").on("click", function(event){
         event.preventDefault();
         var hash = this.hash;
         $('body,html').animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
     });
+
+    htmlMove();
+    cssMove();
+    
+
 })
